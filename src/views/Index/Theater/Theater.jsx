16caeb32213fb25aa2ce2ce9ebe13d779db10bee
Liuxu-web <903547705@qq.com>
2020-05-01 20:20:40 +0800

    注释了队友的代码. 因为打不开项目,请检查你的bug

--- src/views/Index/Theater/Theater.jsx
+++ src/views/Index/Theater/Theater.jsx
@@ -1,17 +1,27 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import { TheStyle } from './theaterStyle'
-import Swiper from 'swiper'
+import React, { Component } from "react";
+import axios from "axios";
+import { TheStyle } from "./theaterStyle";
+import Swiper from "swiper";
 import "../../../../node_modules/swiper/css/swiper.min.css";
 import "../../../assets/css/Home-swiper.css";
 
-let dian = require('../../../assets/img/dian.png')
+// 这个看起来像是图片. 但是指定目录没有
+// let dian = require('../../../assets/img/dian.png')
 
 class theater extends Component {
+  constructor() {
+    // 这个记得放在上面
+    super();
+    this.state = {
+      theatreList: [],
+    };
+  }
+
   render() {
     return (
       <div>
-        <TheStyle>
+        {/* 因为样式的问题, 所以只能把你的写的页面注释了 */}
+        {/* <TheStyle>
           <div className="head_nav">
             <h3>剧院</h3>
           </div>
@@ -51,7 +61,7 @@ class theater extends Component {
                                         <img src={item2.pic} alt="" />
                                       </a>
                                     </div>
-                                  )
+                                  );
                                 })}
                               </div>
                             </div>
@@ -59,42 +69,37 @@ class theater extends Component {
                         </div>
                       </div>
                     </li>
-                  )
+                  );
                 })}
               </ul>
             </div>
           </div>
-        </TheStyle>
+        </TheStyle> */}
       </div>
-    )
+    );
   }
-  constructor() {
-    super()
-    this.state = {
-      theatreList: [],
-    }
-  }
-  componentDidMount() {
-    axios('/api/theatre/index/getTheatreList', {
-      params: {
-        page: 1,
-        version: '6.1.1',
-        referer: 1,
-      },
-    }).then((res) => {
-      console.log(res.data.data.theatre_list)
 
-      this.setState({ theatreList: res.data.data.theatre_list })
-      var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 'auto',
-        spaceBetween: 0,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-      })
-    })
-  }
+  // componentDidMount() {
+  //   axios("/api/theatre/index/getTheatreList", {
+  //     params: {
+  //       page: 1,
+  //       version: "6.1.1",
+  //       referer: 1,
+  //     },
+  //   }).then((res) => {
+  //     console.log(res.data.data.theatre_list);
+
+  //     this.setState({ theatreList: res.data.data.theatre_list });
+  //     var swiper = new Swiper(".swiper-container", {
+  //       slidesPerView: "auto",
+  //       spaceBetween: 0,
+  //       pagination: {
+  //         el: ".swiper-pagination",
+  //         clickable: true,
+  //       },
+  //     });
+  //   });
+  // }
 }
 
-export default theater
+export default theater;
